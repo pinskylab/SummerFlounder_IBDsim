79c064291a2c5e8e771cedbecee4234c53aa3aa3
Jennifer Hoey <jahoey13@gmail.com>
2021-07-06 14:50:24 -0700

    increased buffer from 10% to 40%

--- scripts/gridprep_dist.R
+++ scripts/gridprep_dist.R
@@ -9,14 +9,14 @@ meta <- fread(here("data", "pade_metadata.txt"))
 xy <- meta[, .(PinskyID, lon, lat, dist)] # dist = great circle distance from a southern point (km)
 max(xy$dist)
 
-# Aim for grid with 1 million points, 10% buffer on all edges
+# Aim for grid with 1 million points, 40% buffer on all edges
 # We'll use 200 x 4000: width is 5% of length
 # to match 100 km wide x 1900 km continental shelf on east coast of us
-# So buffer of 400 on either end of the y-direction
-xy$grid_y <- round((xy$dist - min(xy$dist))/diff(range(xy$dist))*3200+400)
+# So buffer of 1600 on either end of the y-direction
+xy$grid_y <- round((xy$dist - min(xy$dist))/diff(range(xy$dist))*800+1600)
 xy$grid_x <- 100 # all samples in the middle
 
-# y grid range for samples should be 400 to 3600
+# y grid range for samples should be 1600 to 2400
 range(xy$grid_y)
 
 # Space the samples away from the center line if they share the same y-coord
@@ -31,9 +31,11 @@ for(i in 1:length(ys)){
 # Plot
 xy[, plot(grid_x, grid_y)]
 
+# x grid range for samples should be 80 to 120
+range(xy$grid_x)
 
 # Location of Cape Hatteras in grid coords
-round((900 - min(xy$dist))/diff(range(xy$dist))*3200+400) # 1837
+round((864 - min(xy$dist))/diff(range(xy$dist))*800+1600) # 1944; 864 and below is south of Hatteras while any distance above 864 is north of Hatteras
 
 # write out
-write.csv(xy[order(grid_y), .(PinskyID, grid_x, grid_y)], file = here('output', 'pade_gridlocations.csv'), row.names = FALSE)
+write.csv(xy[order(grid_y), .(PinskyID, grid_x, grid_y)], file = here('output', 'pade_gridlocations_buffer40.csv'), row.names = FALSE)
